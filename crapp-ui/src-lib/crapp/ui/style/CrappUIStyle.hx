127dda1ac5c07a0c9ff6290393403146d8927789
Felipe Peternella <felipevex@gmail.com>
2024-07-05 10:31:45 -0300

    typedef para estilo para tornar a criacao de estilos mais conveniente

--- crapp-ui/src-lib/crapp/ui/style/CrappUIStyle.hx
+++ crapp-ui/src-lib/crapp/ui/style/CrappUIStyle.hx
@@ -4,6 +4,8 @@ import priori.style.font.PriFontStyle;
 import priori.geom.PriColor;
 
 class CrappUIStyle {
+
+    public var data(get, set):CrappUIStyleData;
     
     public var primary:CrappUIColor;
     public var background:CrappUIColor;
@@ -32,6 +34,32 @@ class CrappUIStyle {
         this.corners = corners;
     }
 
+    private function get_data():CrappUIStyleData {
+        return {
+            primary : this.primary,
+            background : this.background,
+            size : this.size,
+            space : this.space,
+            fontFamily : this.fontFamily,
+            corners : this.corners,
+            selectedColorWeight : this.selectedColorWeight
+        };
+    }
+
+    private function set_data(value:CrappUIStyleData):CrappUIStyleData {
+        if (value == null) return value;
+
+        if (value.primary != null) this.primary = value.primary;
+        if (value.background != null) this.background = value.background;
+        if (value.size != null) this.size = value.size;
+        if (value.space != null) this.space = value.space;
+        if (value.fontFamily != null) this.fontFamily = value.fontFamily;
+        if (value.corners != null) this.corners = value.corners;
+        if (value.selectedColorWeight != null) this.selectedColorWeight = value.selectedColorWeight;
+
+        return value;
+    }
+
     inline public function selectedBackgroundColor():CrappUIColor return new CrappUIColor(this.background.darken(this.selectedColorWeight));
 
     static public function bluePrint():CrappUIStyle {
@@ -50,6 +78,12 @@ class CrappUIStyle {
             this.primary.color, 
             this.fontFamily
         );
+    }
+
+    static public function fromData(data:CrappUIStyleData):CrappUIStyle {
+        var result:CrappUIStyle = CrappUIStyle.bluePrint();
+        result.data = data;
+        return result;
     }
 
 }
---
+++ crapp-ui/src-lib/crapp/ui/style/CrappUIStyleData.hx
@@ -0,0 +1,11 @@
+package crapp.ui.style;
+
+typedef CrappUIStyleData = {
+    @:optional var primary: CrappUIColor;
+    @:optional var background: CrappUIColor;
+    @:optional var size: Float;
+    @:optional var space: Float;
+    @:optional var fontFamily: String;
+    @:optional var corners: Float;
+    @:optional var selectedColorWeight: Float;
+}
