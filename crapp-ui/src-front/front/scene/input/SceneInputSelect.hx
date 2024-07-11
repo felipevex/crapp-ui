745eb8b3c0ccd10f34ec2cf4613baf473889a94b
Felipe Peternella <felipevex@gmail.com>
2024-07-11 19:23:16 -0300

    novo uso do xml do builder para instanciar objetos tipados

--- crapp-ui/src-front/front/scene/input/SceneInputSelect.hx
+++ crapp-ui/src-front/front/scene/input/SceneInputSelect.hx
@@ -8,11 +8,20 @@ import crapp.ui.display.app.CrappUIScene;
 <priori>
     <imports>
         <crapp.ui.display.layout.CrappUILayotable />
+        <crapp.ui.display.input.CrappUISelectInput />
     </imports>
     <view>
         <private:CrappUILayotable id="container" hLayoutAlignment="CENTER" vLayoutDistribution="SIDE" vLayoutGap="10" left="10" right="10" top="10" bottom="10" >
+            <CrappUISelectInput id="select1" data:Literal="this.selectData" type="<InputSelectData>" />
+            <CrappUISelectInput id="select2" data:Literal="this.selectData" type="<InputSelectData>" label="Write Other Label" />
+            <CrappUISelectInput id="select3" data:Literal="this.selectData" type="<InputSelectData>" label="Change the Value Label" labelField="value" />
+            <CrappUISelectInput id="select4" data:Literal="this.selectData" type="<InputSelectData>" />
+            <CrappUISelectInput id="select5" data:Literal="this.selectData" type="<InputSelectData>" />
+            <CrappUISelectInput id="select6" data:Literal="this.selectData" type="<InputSelectData>" />
             <private:CrappUILayotable id="distribute" vLayoutSize="FIT" hLayoutSize="FLEX" hLayoutDistribution="SIDE" hLayoutGap="10" >
-                
+                <CrappUISelectInput type="<InputSelectData>" hLayoutSize="FLEX" />
+                <CrappUISelectInput type="<InputSelectData>" hLayoutSize="FLEX" />
+                <CrappUISelectInput type="<InputSelectData>" hLayoutSize="FLEX" />
             </private:CrappUILayotable>
         </private:CrappUILayotable>
     </view>
@@ -20,71 +29,31 @@ import crapp.ui.display.app.CrappUIScene;
 ')
 class SceneInputSelect extends CrappUIScene {
 
+    var selectData:Array<InputSelectData> = [
+        { label: "Option 1", value: 1 },
+        { label: "Option 2", value: 2 },
+        { label: "Option 3", value: 3 },
+        { label: "Option 4", value: 4 },
+        { label: "Option 5", value: 5 },
+        { label: "Option 6", value: 6 },
+        { label: "Option 7", value: 7 }
+    ];
+
     override function setup() {
 
         var style:CrappUIStyle = CrappUIStyle.bluePrint();
         style.background = 0xFF0000;
         style.primary = 0xFFFFFF;
 
-        var selectData:Array<InputSelectData> = [
-            { label: "Option 1", value: 1 },
-            { label: "Option 2", value: 2 },
-            { label: "Option 3", value: 3 },
-            { label: "Option 4", value: 4 },
-            { label: "Option 5", value: 5 },
-            { label: "Option 6", value: 6 },
-            { label: "Option 7", value: 7 }
-        ];
-
-        var select1:CrappUISelectInput<InputSelectData> = new CrappUISelectInput<InputSelectData>();
-        select1.data = selectData;
-
-        var select2:CrappUISelectInput<InputSelectData> = new CrappUISelectInput<InputSelectData>();
-        select2.label = "Write Other Label";
-        select2.data = selectData;
-
-        var select3:CrappUISelectInput<InputSelectData> = new CrappUISelectInput<InputSelectData>();
-        select3.data = selectData;
-        select3.label = "Change the Value Label";
-        select3.labelField = "value";
-
-        var select4:CrappUISelectInput<InputSelectData> = new CrappUISelectInput<InputSelectData>();
-        select4.data = selectData;
         select4.style = style;
 
-        var select5:CrappUISelectInput<InputSelectData> = new CrappUISelectInput<InputSelectData>();
-        select5.data = selectData;
         select5.actions.onChange = () -> {
             select5.label = select5.value.label;
         }
 
-        var select6:CrappUISelectInput<InputSelectData> = new CrappUISelectInput<InputSelectData>();
-        select6.data = selectData;
         select6.actions.onDelayedChange = () -> {
             select6.label = select6.value.label;
         }
-
-        this.container.addChildList([
-            select1,
-            select2,
-            select3,
-            select4,
-            select5,
-            select6
-        ]);
-
-        this.distribute.addChildList([
-            this.createFlexInput(),
-            this.createFlexInput(),
-            this.createFlexInput()
-        ]);
-    }
-
-    private function createFlexInput():CrappUISelectInput<InputSelectData> {
-        var result = new CrappUISelectInput<InputSelectData>();
-        result.hLayoutSize = FLEX;
-        
-        return result;
     }
 
 }
