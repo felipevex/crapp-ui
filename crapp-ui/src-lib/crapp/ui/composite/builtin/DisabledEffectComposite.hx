1471536826652949f300e5c02958c709e7b5ba48
Felipe Peternella <felipevex@gmail.com>
2025-01-03 17:55:17 -0300

    Low alpha for disabled effect composite

--- crapp-ui/src-lib/crapp/ui/composite/builtin/DisabledEffectComposite.hx
+++ crapp-ui/src-lib/crapp/ui/composite/builtin/DisabledEffectComposite.hx
@@ -2,8 +2,18 @@ package crapp.ui.composite.builtin;
 
 class DisabledEffectComposite extends CrappUIComposite {
     
+    @:isVar public var lowAlpha(default, set):Float = 0.5;
+
     public function updateDisplay():Void {
-        this.display.alpha = this.display.disabled ? 0.5 : 1;
+        this.display.alpha = this.display.disabled ? this.lowAlpha : 1;
+    }
+
+    private function set_lowAlpha(value:Float):Float {
+        if (value == null) return value;
+        
+        this.lowAlpha = value;
+        this.updateDisplay();
+        return value;
     }
 
 }
