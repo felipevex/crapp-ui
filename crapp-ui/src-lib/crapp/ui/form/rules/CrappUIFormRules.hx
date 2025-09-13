40ed4644252b4d67c0f793d9d96fbe5fe8aa90b7
Felipe Peternella <felipevex@gmail.com>
2025-09-13 10:11:05 -0300

    A validação de email deve ocorrer somente se a string nao for vazia

--- crapp-ui/src-lib/crapp/ui/form/rules/CrappUIFormRules.hx
+++ crapp-ui/src-lib/crapp/ui/form/rules/CrappUIFormRules.hx
@@ -6,16 +6,17 @@ enum abstract CrappUIFormRules(String) {
 
     var REQUIRED;
     var IS_EMAIL;
-    
+
     @:to
     private function toValidator<T>():(value:T)->Void {
         return switch (cast this) {
-            
+
             case REQUIRED : (value:T) -> {
                 if (value == null || StringKit.trim(Std.string(value)).length == 0) throw 'Campo obrigatório';
             }
 
             case IS_EMAIL : (value:T) -> {
+                if (value != null && StringKit.isEmpty(Std.string(value))) return;
                 if (value != null && !StringKit.isEmail(Std.string(value))) throw 'Email inválido';
             }
 
